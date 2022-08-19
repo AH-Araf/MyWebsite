59c3ed0ed6a0dd60d8eeb803a0faf4f685a85c1f
Araf <arafathossain.baiust@gmail.com>
2022-08-20 03:01:43 +0600

    Dark & Day Mode

---
+++ js/style.js
@@ -0,0 +1,10 @@
+
+document.getElementById('btn-dark').addEventListener('click',function(){
+    document.body.style.backgroundColor= '#1a1728';
+    document.body.style.color= 'white';
+    
+});
+document.getElementById('btn-normal').addEventListener('click',function(){
+    document.body.style.backgroundColor= 'white';
+    document.body.style.color= 'black';
+})
